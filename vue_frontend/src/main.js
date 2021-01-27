d07e6b653e6d9735f181556f0898084850e21a59
artur <arturwodroid@gmail.com>
2021-01-27 18:06:28 -0300

    bootstrap configurado

--- vue_frontend/src/main.js
+++ vue_frontend/src/main.js
@@ -1,15 +1,11 @@
 import Vue from 'vue'
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import IdleVue from 'idle-vue'
-
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import './main.scss'
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
