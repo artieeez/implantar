7cfd1189b277b45a9c2039f66d07c4941292050b
artur <arturwodroid@gmail.com>
2021-02-02 11:53:55 -0300

    Simplejwt bugfix

--- implantar_backend/checker/admin.py
+++ implantar_backend/checker/admin.py
@@ -11,4 +11,17 @@ admin.site.register(Categoria)
 admin.site.register(Profile)
 
 
+# FIX sem permissão para remover usuário por conta do OutstandingToken
+# # Abordado em:
+# # # https://github.com/SimpleJWT/django-rest-framework-simplejwt/issues/266
+
+from rest_framework_simplejwt.token_blacklist.models import OutstandingToken
+from rest_framework_simplejwt.token_blacklist.admin import OutstandingTokenAdmin
+
+class CustomOutstandingTokenAdmin(OutstandingTokenAdmin):
+    def has_delete_permission(self, *args, **kwargs):
+        return True
+
+admin.site.unregister(OutstandingToken)
+admin.site.register(OutstandingToken, CustomOutstandingTokenAdmin)
 
