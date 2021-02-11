6d2031bc6ae02ea6d001487fa66722e1b5bee1b6
artur <arturwodroid@gmail.com>
2021-02-11 15:27:20 -0300

    throttling

--- implantar_backend/implantar_backend/settings.py
+++ implantar_backend/implantar_backend/settings.py
@@ -150,6 +150,14 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '60/minute',
+        'user': '1000/day'
+    }
 }
 
 AVALIADOR_GROUP_NAME = 'avaliador'
